83ce3d8cd83e1fd1e272ecfe17038e5bfe726c99
binarygary <gkovar@gmail.com>
2016-01-08 15:09:17 -0500

    odds and ends

--- unsticky.php
+++ unsticky.php
@@ -13,45 +13,56 @@
 
 
 //wp_schedule_single_event( time() + 3600, 'my_new_event', array( $arg1, $arg2, $arg3 ) );
+//current_time();
+//wp_checkdate();
 
+//unstick_post($post_id);
 
 add_action( 'post_submitbox_misc_actions', 'unsticky' );
 
 function unsticky() {
-    global $post;
+	global $post;
+		
     if (get_post_type($post) == 'post' && is_sticky($post->ID) ) {
 		
         echo '<div class="misc-pub-section misc-pub-section-last" style="border-top: 1px solid #eee;">';
-        wp_nonce_field( plugin_basename(__FILE__), 'unsticky_nonce' );
-        if ( true === get_post_meta( $post->ID, '_unsticky_time') ) {
-        	$unstickyTime = get_post_meta( $post->ID, '_unsticky_time', true );
-        }
-		
+        $offset = get_option( 'gmt_offset' ) * HOUR_IN_SECONDS;
+		$whenToUnstick=time()+$offest;
+		if ( true == get_post_meta( $post->ID, '_unsticky_time') ) {
+        	$whenToUnstick = get_post_meta( $post->ID, '_unsticky_time', true );
+		}
+		$month=date('F',$whenToUnstick);
+		$year=date('Y',$whenToUnstick);
+		$day=date('j',$whenToUnstick);
+		$hour=date('G',$whenToUnstick);
+		$minute=date('i',$whenToUnstick);
+		$second=date('s',$whenToUnstick);
 		?>
 		<h4>Schedule When to Unsticky:</h4>
-		<span class="screen-reader-text">Scheduled Unsticky Date and Time</span></a>
-					<fieldset id="timestampdiv">
-					<legend class="screen-reader-text">Scheduled Unsticky Date and Time</legend>
-					<div class="timestamp-wrap"><label><span class="screen-reader-text">Month</span>
-					<select id="mm" name="mm">
-					<option value="01" data-text="Jan" >01-Jan</option>
-					<option value="02" data-text="Feb" >02-Feb</option>
-					<option value="03" data-text="Mar" >03-Mar</option>
-					<option value="04" data-text="Apr" >04-Apr</option>
-					<option value="05" data-text="May" >05-May</option>
-					<option value="06" data-text="Jun" >06-Jun</option>
-					<option value="07" data-text="Jul" >07-Jul</option>
-					<option value="08" data-text="Aug" >08-Aug</option>
-					<option value="09" data-text="Sep" >09-Sep</option>
-					<option value="10" data-text="Oct" >10-Oct</option>
-					<option value="11" data-text="Nov" >11-Nov</option>
-					<option value="12" data-text="Dec" >12-Dec</option>
-					</select></label> <label><span class="screen-reader-text">Day</span>
-						<input type="text" id="jj" name="jj" value="08" size="2" maxlength="2" autocomplete="off" /></label>, <label><span class="screen-reader-text">Year</span>
-							<input type="text" id="aa" name="aa" value="2016" size="4" maxlength="4" autocomplete="off" /></label> @ <label><span class="screen-reader-text">Hour</span>
-								<input type="text" id="hh" name="hh" value="02" size="2" maxlength="2" autocomplete="off" /></label>:<label><span class="screen-reader-text">Minute</span>
-									<input type="text" id="mn" name="mn" value="07" size="2" maxlength="2" autocomplete="off" /></label></div>
-									<input type="hidden" id="ss" name="ss" value="22" />
+		<span class="screen-reader-text">Scheduled Unsticky Date and Time</span>
+		<fieldset id="timestampdiv">
+		<legend class="screen-reader-text">Scheduled Unsticky Date and Time</legend>
+		<div class="timestamp-wrap"><label><span class="screen-reader-text">Month</span>
+		<select id="month" name="month">
+		<?php
+		for ($i=1; $i<13; $i++) {
+			$numberMonth=$i;
+			if (strlen($numberMonth)<2) {
+				$numberMonth="0"."$numberMonth";
+			}
+			echo "<option value=$numberMonth ";
+			if ((int)$month === (int)$numberMonth) {
+				echo "selected";
+			}
+			echo ">$numberMonth</option>";
+		}
+		?>
+		</select></label> <label><span class="screen-reader-text">Day</span>
+		<input type="text" id="day" name="day" value="<?php echo $day; ?>" size="2" maxlength="2" autocomplete="off" /></label>, <label><span class="screen-reader-text">Year</span>
+		<input type="text" id="year" name="year" value="<?php echo $year; ?>" size="4" maxlength="4" autocomplete="off" /></label> @ <label><span class="screen-reader-text">Hour</span>
+		<input type="text" id="hour" name="hour" value="<?php echo $hour; ?>" size="2" maxlength="2" autocomplete="off" /></label>:<label><span class="screen-reader-text">Minute</span>
+		<input type="text" id="minute" name="minute" value="<?php echo $minute; ?>" size="2" maxlength="2" autocomplete="off" /></label></div>
+		<input type="hidden" id="second" name="second" value="<?php echo $second; ?>" />
 		<?php
 		//if it's not set then display standard format
 		
@@ -60,28 +71,27 @@ function unsticky() {
 }
 
 add_action( 'save_post', 'save_unsticky' );
-
 function save_unsticky( $post_id ) {
 
-    if (!isset($_POST['post_type']) )
-        return $post_id;
+	//print_r($_POST);
+	
+    //if (!isset($_POST['post_type']) )
+    //    return $post_id;
 
-    if ( !wp_verify_nonce( $_POST['unsticky_nonce'], plugin_basename(__FILE__) ) )
-        return $post_id;
+    //if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) 
+    //    return $post_id;
 
-    if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) 
-        return $post_id;
-
-    if ( 'post' == $_POST['post_type'] && !current_user_can( 'edit_post', $post_id ) )
-        return $post_id;
+    //if ( 'post' == $_POST['post_type'] && !current_user_can( 'edit_post', $post_id ) )
+    //    return $post_id;
     
-    if (!isset($_POST['']))
-        return $post_id;
-    else {
-		print_r($_POST);
-        $mydata = $_POST['article_or_box'];
-        update_post_meta( $post_id, '_article_or_box', $_POST['article_or_box'], get_post_meta( $post_id, '_article_or_box', true ) );
-    }
+    //if (!isset($_POST['unsticky_nonce'])) {
+	//	return $post_id;
+	//} else {
+		$unstickyTime=strtotime("$_POST[month]/$_POST[day]/$_POST[year] $_POST[hour]:$_POST[minute]:$_POST[second]");
+		update_post_meta( $post_id, '_unsticky_time', $unstickyTime );
+		//print_r($_POST);
+		return $post_id;
+		//}
 
 }
 	
